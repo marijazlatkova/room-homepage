9e3100ffc2ee21f7bdeaccc89cd8ca51f21b16da
Marija Zlatkova <marijazlatkova44@yahoo.com>
2024-10-25 15:09:04 +0200

    data: add images and content

---
+++ src/data/images.js
@@ -0,0 +1,8 @@
+export const images = {
+  hamburger: "/images/icon-hamburger.svg",
+  close: "/images/icon-close.svg",
+  logo: "/images/logo.svg",
+  left: "/images/icon-angle-left.svg",
+  right: "/images/icon-angle-right.svg",
+  arrow: "/images/icon-arrow.svg"
+};
